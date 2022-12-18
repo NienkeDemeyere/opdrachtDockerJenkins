57a2c328d00e71e6c68d242c596ede01d2d83154
NienkeDemeyere <nienke.demeyere@student.vives.be>
2022-12-18 12:22:37 +0100

    test foor webhook

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send("Hello World from Nienke's server");
 });
-
+app.get('/test', (req, res) => {
+  res.send("this is a test for webhook");
+});
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}`);
 });
